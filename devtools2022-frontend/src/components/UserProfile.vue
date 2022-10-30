ec511e09eba009727742ccbcfa159ce1fd38398e
Ubuntu <ubuntu@ip-172-31-44-1.ap-southeast-1.compute.internal>
2022-10-30 06:16:09 +0000

    Added initial project

---
+++ devtools2022-frontend/src/components/UserProfile.vue
@@ -0,0 +1,24 @@
+<template>
+  <header>
+    <h1>User</h1>
+    <Button text="Sign up" />
+    <Button text="Login" />
+  </header>
+</template>
+
+<script>
+import Button from "./Button.vue";
+export default {
+  name: "UserProfile",
+  props: {},
+  components: {
+    Button,
+  },
+};
+</script>
+
+<style scoped>
+header {
+  display: flex;
+}
+</style>
